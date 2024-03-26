9fa4ebb1b40da25499f067f8288c13c44a3d3e00
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-03-26 10:53:10 +0100

    first commit

---
+++ Checkpoint5.py
@@ -0,0 +1,31 @@
+# Ejercicio 1
+
+words = ["Rubens", "Ballester", "Lillo"]
+
+for word in words:
+    print(word)
+
+
+# Ejercicio 2
+    
+def suma(a, b, c):
+    return a + b + c
+
+# Ejercicio 3
+
+suma = lambda a, b, c: a + b + c
+
+# Ejercicio 4
+
+nombre = "Enrique"
+
+lista_nombre = ["Jessica", "Paul", "George", "Henry", "Adán"]
+
+for i in lista_nombre:
+    if nombre == i:
+        print(f"El nombre {nombre} coincide con un nombre de la lista")
+        break
+else:
+    print(f"El nombre {nombre} no está en la lista") 
+        
+         
